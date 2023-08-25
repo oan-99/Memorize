b52612d72d26fb4295fda5a191cf192ab551c8ab
oan-99 <oansc_99@yahoo.com>
2023-08-26 02:24:04 +0500

    Cards implemented. Sample button implemented.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,14 +9,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            CardView(faceDown: false)
+            CardView()
+            CardView()
+            Spacer()
+            CardButtons()
         }
         .padding()
     }
 }
+
+struct CardView: View {
+    @State var faceDown : Bool = true
+    var body: some View {
+        let base = RoundedRectangle(cornerRadius: 5)
+        ZStack{
+            if(faceDown){
+                base.fill(.orange)
+            }
+            else {
+                base.fill(.white)
+                base.stroke(.orange)
+                Text("🦩")
+            }
+        }
+        .padding()
+        
+    }
+}
+
+struct CardButtons: View {
+    var body: some View {
+        Button("Add/Remove Button") {
+            
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
